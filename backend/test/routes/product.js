50687b56b111307aa38be31169e00f5b6dbe432c
Edmil Di Massimo <edmildm@gmail.com>
2019-08-07 10:03:14 -0400

    Added authentication to tests.

--- backend/test/routes/product.js
+++ backend/test/routes/product.js
@@ -4,12 +4,22 @@ const axios = require('axios');
 
 const env = require('../../config/env');
 
-const endpoint = `http://localhost:${env.PORT}/api`;
+const endpoint = `http://localhost:${env.PORT}/api/v1`;
 
 describe('', () => {
   it('Get all products', async () => {
     try {
-      const data = await axios.get(`${endpoint}/products`);
+      let loginData = {
+        username: 'test',
+        password: 'test'
+      };
+      const auth = await axios.post(`${endpoint}/login`, loginData);
+      let config = {
+        headers: {
+          Authorization: `Token ${auth.data.user.token}`,
+        }
+      }
+      const data = await axios.get(`${endpoint}/products`, config);
       assert.equal(data.status, 200);
       assert(data.data);
     } catch (e) {
@@ -18,13 +28,23 @@ describe('', () => {
   }).timeout(5000);
 
   it('Create a product', async () => {
+    let loginData = {
+      username: 'test',
+      password: 'test'
+    };
+    const auth = await axios.post(`${endpoint}/login`, loginData);
+    let config = {
+      headers: {
+        Authorization: `Token ${auth.data.user.token}`,
+      }
+    }
     const testData = {
       name: 'CreateProduct Test',
       description: 'Unit test',
       quantity: 0,
     };
     try {
-      const data = await axios.post(`${endpoint}/create-product`, testData);
+      const data = await axios.post(`${endpoint}/create-product`, testData, config);
       assert.equal(data.status, 200);
       assert(data.data);
     } catch (e) {
